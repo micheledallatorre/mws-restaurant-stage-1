e3062fcd93241a8ef8402e46522f492db2d98dfc
Michele Dalla Torre <micheledallatorre@gmail.com>
2018-05-08 17:21:01 +0200

    A11y accessibility features #5

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -51,16 +51,20 @@ fetchRestaurantFromURL = (callback) => {
 fillRestaurantHTML = (restaurant = self.restaurant) => {
   const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
+  name.tabIndex = '0';
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  address.tabIndex= '0';
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
+  image.setAttribute('alt','Picture of restaurant ' + restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.tabIndex= '0';
 
   // fill operating hours
   if (restaurant.operating_hours) {
@@ -80,13 +84,16 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 
     const day = document.createElement('td');
     day.innerHTML = key;
+    day.tabIndex= '0';
     row.appendChild(day);
 
     const time = document.createElement('td');
     time.innerHTML = operatingHours[key];
+    time.tabIndex= '0';
     row.appendChild(time);
 
     hours.appendChild(row);
+    hours.tabIndex= '0';
   }
 }
 
@@ -97,11 +104,13 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+  title.tabIndex= '0';
   container.appendChild(title);
 
   if (!reviews) {
     const noReviews = document.createElement('p');
     noReviews.innerHTML = 'No reviews yet!';
+    noReviews.tabIndex= '0';
     container.appendChild(noReviews);
     return;
   }
@@ -121,21 +130,25 @@ createReviewHTML = (review) => {
   const date = document.createElement('p');
   date.innerHTML = review.date;
   date.className = 'review-date';
+  date.tabIndex= '0';
   li.appendChild(date);
 
   const name = document.createElement('p');
   name.innerHTML = review.name;
   name.className = 'review-author';
+  name.tabIndex= '0';
   li.appendChild(name);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
   rating.className = 'review-rating';
+  rating.tabIndex= '0';
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
   comments.className = 'review-comment';
+  comments.tabIndex= '0';
   li.appendChild(comments);
 
   return li;
@@ -148,6 +161,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute("aria-current", "page");
   breadcrumb.appendChild(li);
 }
 
