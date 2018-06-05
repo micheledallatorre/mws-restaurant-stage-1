5a7c66d985066768bdc4d75517f965beb19b9691
Michele Dalla Torre <micheledallatorre@gmail.com>
2018-06-05 15:42:32 +0200

    add gulp sass, autoprefixer, browser sync, eslint #9

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,53 @@
+/*eslint-env node */
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+ //browserSync.stream();
+var eslint = require('gulp-eslint');
 
-gulp.task('default', defaultTask);
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+
+
+gulp.task('styles', function(done) {
+	// look for .scss files in sass folder and subdirs
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
+
+	console.log("Css generated in /css folder!");
+	done();
+});
+
+
+gulp.task('lint', () => {
+	// ESLint ignores files with "node_modules" paths.
+	// So, it's best to have gulp ignore the directory as well.
+	// Also, Be sure to return the stream from the task;
+	// Otherwise, the task may end before the stream has finished.
+	return gulp.src(['**/*.js','!node_modules/**'])
+		// eslint() attaches the lint output to the "eslint" property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failAfterError last.
+		.pipe(eslint.failAfterError());
+
+});
+
+gulp.task('default', gulp.series(['styles', 'lint'], function() {
+	gulp.watch('sass/**/*.scss', gulp.series('styles'));
+	gulp.watch('js/**/*.js', gulp.series('lint'));
+
+	browserSync.init({
+		server: "./"
+	});
+
+}));
